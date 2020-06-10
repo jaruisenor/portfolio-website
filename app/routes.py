e1f676d1962076154a364aca34e8b3bbec18a302
jaruisenor <jaruisenor@uc.cl>
2020-06-09 22:22:59 -0400

    Title

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ import os
 @app.route("/", methods=['POST', 'GET'])
 @app.route("/index", methods=['POST', 'GET'])
 def index():
-
-    return render_template('index.html', title_text='Titulo Temporal', title="DATA SCIENCE & SUSTAINABILITY", id="index")
+    Texto = "I’m an engineer undergraduate (soon to be graduate) with a back ground in mining and industrial engineering. I’m always looking to solve real world problems with an analytical perspective with data analytics, machine learning and data visualization tools at hand. The mix between business oriented formation and Data Analytics technical skills, I believe, makes a difference in todays world. This portfolio contains personal projects done in my leisure time involving Data Science."
+    return render_template('index.html', title_text=Texto, title="DATA SCIENCE & WEB DEVELOPMENT", id="index")
 
 
